c0bd39ba438bfd8f82df368097aa0e16cb2f87e7
pthorsson <p.thorsson@gmail.com>
2019-04-01 23:22:11 +0200

    Added cipher functions

---
+++ src/lib/auth.ts
@@ -0,0 +1,39 @@
+import { createCipher, createDecipher } from 'crypto';
+
+const ALGORITHM = 'aes192';
+const HASH_FORMAT = 'hex';
+const ENCODING = 'utf8';
+
+/**
+ * Creates a AES192 cipher from given content string and password.
+ * 
+ * @param content String to be enchipered.
+ * @param password Password key to encipher string.
+ * 
+ * @returns {string} Cipher hash.
+ */
+export const cipher = (content: string, password: string): string => {
+  const cipher = createCipher(ALGORITHM, password);
+
+  let encrypted = cipher.update(content, ENCODING, HASH_FORMAT);
+  encrypted += cipher.final(HASH_FORMAT);
+
+  return encrypted;
+}
+
+/**
+ * Deciphers given AES192 cipher hash with given password.
+ * 
+ * @param hash Hash string to be dechipered.
+ * @param password Password key to decipher hash.
+ * 
+ * @returns {string} Deciphered data or null if wrong password.
+ */
+export const decipher = (hash: string, password: string): string => {
+  const decipher = createDecipher(ALGORITHM, password);
+
+  let decrypted = decipher.update(hash, HASH_FORMAT, ENCODING);
+  decrypted += decipher.final(ENCODING);
+
+  return decrypted;
+}
