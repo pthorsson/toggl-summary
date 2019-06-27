a03f1f66c69226a602090f1fd0504eb222bc3222
pthorsson <p.thorsson@gmail.com>
2019-06-27 23:34:04 +0200

    Added generic elements

--- src/config.ts
+++ src/config.ts
@@ -4,3 +4,6 @@ export const COLOR_BLUE = '#6aa2ed';
 export const COLOR_RED = '#f26060';
 export const COLOR_ORANGE = '#ff9c60';
 export const COLOR_GREEN = '#35a86b';
+
+// Font
+export const FONT_FAMILY = 'system, -apple-system, ".SFNSText-Regular", "San Francisco", "Roboto", "Segoe UI", "Helvetica Neue", "Lucida Grande", sans-serif';
--- src/elements.tsx
+++ src/elements.tsx
@@ -1,6 +1,6 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { lighten } from 'polished';
-import { COLOR_GRAY } from 'config';
+import { FONT_FAMILY, COLOR_GRAY, COLOR_RED } from 'config';
 
 export const Container = styled.div`
   min-width: 800px;
@@ -26,17 +26,68 @@ export const Button = styled.button`
   margin-right: 2px;
 
   :hover {
-    background-color: ${lighten(.2, COLOR_GRAY)};
+    background-color: ${lighten(0.2, COLOR_GRAY)};
     cursor: pointer;
   }
 
   :disabled {
-    background-color: ${lighten(.5, COLOR_GRAY)};
-    color: ${lighten(.67, COLOR_GRAY)};
+    background-color: ${lighten(0.5, COLOR_GRAY)};
+    color: ${lighten(0.67, COLOR_GRAY)};
     cursor: default;
   }
 
   :focus {
     outline: 0;
   }
+`;
+
+export const Input: any = styled.input`
+  color: ${COLOR_GRAY};
+  border: 2px solid ${COLOR_GRAY};
+  padding: 10px 15px;
+  font-family: ${FONT_FAMILY};
+  display: block;
+  width: 100%;
+  margin-bottom: 15px;
+
+  :hover {
+    border-color: ${lighten(0.2, COLOR_GRAY)};
+  }
+
+  :disabled {
+    color: ${lighten(0.67, COLOR_GRAY)};
+    border-color: ${lighten(0.5, COLOR_GRAY)};
+  }
+
+  :focus {
+    outline: 0;
+  }
+
+  ::placeholder {
+    opacity: 0.3;
+  }
+
+  ${(props: any) =>
+    props.hasError &&
+    css`
+      border-color: ${COLOR_RED} !important;
+    `}
+`;
+
+export const InputLabel: any = styled.label`
+  font-size: 85%;
+  display: block;
+  margin-bottom: 5px;
+  text-transform: uppercase;
+
+  ${(props: any) =>
+    props.hasError &&
+    css`
+      color: ${COLOR_RED} !important;
+    `}
+`;
+
+export const FormWrapper = styled.div`
+  max-width: 400px;
+  margin: 0 auto;
 `;
